0fe61a5565a5443e9b1238e22d9d87ca77c73c36
Bhanupratap10 <nishadbhanupratap0@gmail.com>
2025-06-29 23:31:15 +0530

    first commit

--- Function_or_method/check_prime_number.java
+++ Function_or_method/check_prime_number.java
@@ -1,16 +1,13 @@
 public class check_prime_number {
-    
+  
     public static boolean isPrime(int n) {
-        boolean isPrime = true;
         for(int i=2; i<=n-1; i++){
           if (n % i == 0 ) { // completely dividing
-
-            isPrime = false;
-              break;
+          return false;
           }   
         }
 
-      return isPrime;
+      return true;
     }
     
 
