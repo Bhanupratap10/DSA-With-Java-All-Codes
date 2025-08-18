5ef86e33bcb0e20da172c007886c0e56516c3b87
Bhanupratap10 <nishadbhanupratap0@gmail.com>
2025-08-18 22:53:24 +0530

    first

---
+++ Recursion2/Binary_string.java
@@ -0,0 +1,27 @@
+package Recursion2;
+
+public class Binary_string {
+
+    public static void printBinaryString(int n, int lastPlace, String str ) {
+        // base case 
+
+        if (n == 0 ) {
+            System.out.println(str);
+            return;
+        }
+
+        // kaam 
+
+        printBinaryString(n-1, 0, str.append(0));
+        if(lastPlace == 0) {
+            printBinaryString(n-1, 1, str.append(1));
+        }
+
+    }
+
+    public static void main(String[] args) {
+        printBinaryString(3, 0, new String());
+        
+    }
+    
+}
