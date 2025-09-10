00e2e46b4e5be22c2f9f5ff18e2524732911fc15
Bhanupratap10 <nishadbhanupratap0@gmail.com>
2025-09-10 21:16:07 +0530

    first

--- Arithmetictext.java
+++ Arithmetictext.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Arithmetictext {
     public static void main(String[] args) {
         int A = 39;
-        int B = 34;
+        int B = 39;
         System.out.println("add = " + (A+B));
 
     }
