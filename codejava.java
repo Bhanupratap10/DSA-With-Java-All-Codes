51e6e86fb6f78a88f6ef8f1ca6a2ee4099ea8c94
Bhanupratap10 <nishadbhanupratap0@gmail.com>
2025-09-16 22:39:43 +0530

    first

--- codejava.java
+++ codejava.java
@@ -1,7 +1,7 @@
 public class codejava {
     public static void main(String[] args) {
         int A = 15;
-        int B = 10;
+        int B = 20;
 
         if (A >= B){
             System.out.println("A largest of 2");
