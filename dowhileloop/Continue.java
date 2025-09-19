a4117a7ceea2f3e67d589df8637ce893d90a46d6
Bhanupratap10 <nishadbhanupratap0@gmail.com>
2025-09-19 21:38:50 +0530

    first

--- dowhileloop/Continue.java
+++ dowhileloop/Continue.java
@@ -2,7 +2,7 @@
 public class Continue {
     
     public static void main(String[] args) {
-        for(int i=1; i<=5; i++){
+        for(int i=1; i<=10; i++){
             if(i == 2){
                 continue;
             }
