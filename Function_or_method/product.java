dd36aa193c028a9134577ef658e32fbcf1f699c4
Bhanupratap10 <nishadbhanupratap0@gmail.com>
2025-11-01 22:15:52 +0530

    first

--- Function_or_method/product.java
+++ Function_or_method/product.java
@@ -6,7 +6,7 @@ public class product {
 
     public static void main(String[] args) {
         int a = 12;
-        int b = 8;
+        int b = 12;
         int prod = multiply(a, b);
         System.out.println("a * b = " + prod);
         prod = multiply(10, 28);
