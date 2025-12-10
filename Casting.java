774c2159edbd496bbc7590419fa23c4cd71152a2
Bhanupratap10 <nishadbhanupratap0@gmail.com>
2025-12-11 00:12:53 +0530

    first

--- Casting.java
+++ Casting.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Casting {
    public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    float number = 25.999f;
+    float number = 25.99999f;
     int number2 = (int) number;
     System.out.println(number2);
 
