7f04351faf99e39fa5f37eaef5f9cfde163059ae
Bhanupratap10 <nishadbhanupratap0@gmail.com>
2025-12-29 23:56:00 +0530

    first

--- Recursion2/Remove_duplicate.java
+++ Recursion2/Remove_duplicate.java
@@ -25,7 +25,7 @@ public class Remove_duplicate {
     }
 
     public static void main(String[] args) {
-        String str = "apnnnnacollege";
+        String str = "apnacollege";
         removeDuplicate(str, 0, new StringBuilder(""), new boolean[29]);
         
     }
